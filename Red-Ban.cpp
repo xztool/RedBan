1b78f96cd35c3a00432f8c443be52f116c01927c
Fz <162455167+xztool@users.noreply.github.com>
2024-08-06 09:51:04 +0800

    Create Red-Ban.cpp

---
+++ Red-Ban.cpp
@@ -0,0 +1,3 @@
+               if (getNameFormat(CurrentAnnouncement + 0x18) != "None") {
+                            AdminCheat("BanPlayer " + to_string(Read<uint32_t>(CurrentAnnouncement + 0x10)) + " 365 RedChat");
+                               }
